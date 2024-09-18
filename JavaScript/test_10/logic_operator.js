8f066bd0aac9236063d42ad199b30aac096f5846
Gustavo Rocha <gurocha05@gmail.com>
2024-09-18 11:14:05 -0300

    Update logic_operator.js

---
+++ JavaScript/test_10/logic_operator.js
@@ -0,0 +1,12 @@
+const finalGrade = 6;
+const absence = 5;
+
+//if (finalGrade < 7 || absence > 4) {
+ //   console.log('You failed')}
+
+ if (finalGrade < 7 && absence > 4) {
+     console.log('You failed')}
+
+else{
+    console.log('You passed')
+}

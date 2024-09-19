a71063d3d5d7d08d58bed86860073a0d36c3fcbd
Gustavo Rocha <gurocha05@gmail.com>
2024-09-19 15:43:55 -0300

    expe.js

---
+++ JavaScript/test_10/expe.js
@@ -0,0 +1,9 @@
+const haveExperience = true;
+const haveDiploma = false;
+
+if (haveExperience || haveDiploma) {
+    console.log("Can have the job")
+} 
+else {
+    console.log("Cannot have the job")
+}
--- JavaScript/test_10/logic_operator.js
+++ JavaScript/test_10/logic_operator.js
@@ -15,3 +15,8 @@ else{
 //true && false = false
 //false && true	= false
 //false && false = false
+
+//true || true	= true
+//true || false	= true
+//false || true	= true
+//false || false = false

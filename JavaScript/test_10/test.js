443471960c7e53a124ff2a568a7b13dd510d8b8a
Gustavo Rocha <gurocha05@gmail.com>
2024-09-23 19:03:39 -0300

    Create test.js

---
+++ JavaScript/test_10/test.js
@@ -0,0 +1,26 @@
+const price = 50;
+const text = price < 50 ? 'valor insuficiente' : 'valor suficiente';
+console.log(texto);
+
+let working = true;
+
+function verifyIfWorking() {
+ if (working === true) {
+   return 'It is working';
+ } else {
+   return 'It is not working';
+ }
+}
+
+console.log(verifyIfWorking());
+
+let estudentAge = 16;
+let needPermission;
+
+if (needPermission < 18) {
+ needPermission = true;
+} else {
+ needPermission = false;
+}
+
+console.log(needPermission);

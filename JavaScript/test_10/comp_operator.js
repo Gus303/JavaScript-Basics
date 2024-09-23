f2d2d2387d7d5585da61ef3d181200d1f2074039
Gustavo Rocha <gurocha05@gmail.com>
2024-09-23 18:53:34 -0300

    Create comp_operator.js

---
+++ JavaScript/test_10/comp_operator.js
@@ -0,0 +1,11 @@
+const passed = true;
+
+if(passed === true) {
+    console.log('Passed');
+}
+
+if('0' == 0) {
+    console.log('Passed in the test')
+} else{
+    console.log('Did not passed in the test')
+}

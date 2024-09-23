4d2badc3d91312989b0588181980a9a500507597
Gustavo Rocha <gurocha05@gmail.com>
2024-09-23 19:18:55 -0300

    Update test.js

--- JavaScript/test_10/test.js
+++ JavaScript/test_10/test.js
@@ -1,26 +1,26 @@
-const price = 50;
-const text = price < 50 ? 'valor insuficiente' : 'valor suficiente';
-console.log(texto);
+// const price = 50;
+// const text = price < 50 ? 'valor insuficiente' : 'valor suficiente';
+// console.log(texto);
 
-let working = true;
+// let working = true;
 
-function verifyIfWorking() {
- if (working === true) {
-   return 'It is working';
- } else {
-   return 'It is not working';
- }
-}
+// function verifyIfWorking() {
+//  if (working === true) {
+//    return 'It is working';
+//  } else {
+//    return 'It is not working';
+//  }
+// }
 
-console.log(verifyIfWorking());
+// console.log(verifyIfWorking());
 
-let estudentAge = 16;
-let needPermission;
+// let estudentAge = 16;
+// let needPermission;
 
-if (needPermission < 18) {
- needPermission = true;
-} else {
- needPermission = false;
-}
+// if (needPermission < 18) {
+//  needPermission = true;
+// } else {
+//  needPermission = false;
+// }
 
-console.log(needPermission);
+// console.log(needPermission);

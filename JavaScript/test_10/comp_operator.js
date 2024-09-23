b1f4316c007c9b1a7314b675f26bdbdd8e384e47
Gustavo Rocha <gurocha05@gmail.com>
2024-09-23 18:54:19 -0300

    Update comp_operator.js

--- JavaScript/test_10/comp_operator.js
+++ JavaScript/test_10/comp_operator.js
@@ -4,7 +4,8 @@ if(passed === true) {
     console.log('Passed');
 }
 
-if('0' == 0) {
+//if('0' == 0) {
+if ('0' === 0) //best way
     console.log('Passed in the test')
 } else{
     console.log('Did not passed in the test')

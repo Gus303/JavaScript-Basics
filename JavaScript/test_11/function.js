a1df177f8e5964e954d6be42881acca2ffae047a
Gustavo Rocha <gurocha05@gmail.com>
2024-10-09 13:42:47 -0300

    Create function.js

---
+++ JavaScript/test_11/function.js
@@ -0,0 +1,6 @@
+function showStudentName(name, grade) {
+   return `The student ${name} has a ${grade} as a final grade`;
+}
+
+console.log(showStudentName('Johan', 10));
+console.log(showStudentName('Gustavo', 6));

cee5689748ec2decdf066950e7f7a5eec710c6ab
Gustavo Rocha <gurocha05@gmail.com>
2024-10-09 13:55:12 -0300

    Create calc.js

---
+++ JavaScript/test_11/calc.js
@@ -0,0 +1,17 @@
+function plusTwoNumbers(numA, numB) {
+    return numA + numB;
+   }
+   
+   plusTwoNumbers(2, 2);
+   
+   const plusResult = plusTwoNumbers(2, 2);
+   console.log(plusResult);
+
+//------------//---------------//
+
+   function divided(dividend, divisor) {
+    return dividend / divisor;
+   }
+   
+   const dividedResult = divided(10, 2);
+   console.log(dividedResult);

7d09522bee0d09658e359663c91b00c0e067c4d4
Gustavo Rocha <gurocha05@gmail.com>
2024-10-10 19:29:16 -0300

    Create calc.js

---
+++ JavaScript/class_2/test_1/calc.js
@@ -0,0 +1,10 @@
+// const test1 = 10;
+// const test2 = 6.5;
+// const test3 = 8;
+// const test4 = 7.5;
+
+const test = [10, 6.5, 8, 7.5];
+
+//const finalGrade = (test1 + test2 + test3 + test4) / 4;
+const finalGrade = (test[0] + test[1] + test[2] + test[3]) / test.length;
+console.log(finalGrade);

37f1bd3ff76159bbd1d4149770ebf0d027bfde2b
Gustavo Rocha <gurocha05@gmail.com>
2024-11-14 16:47:15 -0300

    Create array_of_arrays.js

---
+++ JavaScript/class_2/test_3/array_of_arrays.js
@@ -0,0 +1,10 @@
+const student = ['John', 'Alex', 'Tom', 'Mariah'];
+const grades = [4, 9, 0.5, 7];
+
+const infoList = [student, grades];
+
+console.log(
+    `The student number 1 on the list is: ${infoList[0][1]}, and their grade is: ${infoList[1][1]}.`
+);
+
+console.log(infoList[0]);
